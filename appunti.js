c5b7dce188fb95341a862053456d03a7ffbacb41
DomToretto03 <d.cacciapaglia2@studenti.poliba.it>
2025-06-20 12:37:51 +0200

    salva

---
+++ appunti.js
@@ -0,0 +1,6 @@
+//pagina iniziale notizie varie
+//menu a tendina per selezionare le varie categorie
+//login per accedere a notizie specifiche con degli abbonamenti
+//commenti legati alle notizie
+//bordo della pagina api esterne
+//like alla notizia
