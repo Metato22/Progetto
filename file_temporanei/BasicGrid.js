dcd9ab058824636b55e83455ff055e228495c6c8
tonydemox <antoniodemaso20@gmail.com>
2025-07-08 17:08:44 +0200

    modifica alcuni file

--- file_temporanei/BasicGrid.js
+++ file_temporanei/BasicGrid.js
@@ -6,7 +6,7 @@ import Container from '@mui/material/Container';
 import Cards from './Cards'
 import Typography from "@mui/material/Typography";
 import Grid from '@mui/material/Grid';
-
+import './Style.css'
 
 
 
